d9e7e9effbf310bb9866cd818e43243a4a518970
Muhammad-Hazimi-Yusri <mhby1g21@soton.ac.uk>
2024-07-15 15:47:07 +0100

    changed docker image to use new image w/ monoculardepthboosting

--- scripts/360monodepthexecution/stopDockerContainer.ps1
+++ scripts/360monodepthexecution/stopDockerContainer.ps1
@@ -1,5 +1,5 @@
 # Docker image name or ID
-$imageNameOrId = "b5e1aa49a2493fd74215138ef2c747f8d1a6b532616982ebe5a90745d36a2e94"
+$imageNameOrId = "c5ae0f67376a99d442319a0f033d4943be9d89a75d4e75e3df2b3cecf56ebec0"
 
 # Docker container name or ID
 $containerNameOrId = docker ps --filter "ancestor=$imageNameOrId" --format "{{.ID}}"
--- scripts/360monodepthexecution/triggerinteractive.ps1
+++ scripts/360monodepthexecution/triggerinteractive.ps1
@@ -1,4 +1,4 @@
 # Docker image name or ID
-$imageNameOrId = "b5e1aa49a2493fd74215138ef2c747f8d1a6b532616982ebe5a90745d36a2e94"
+$imageNameOrId = "c5ae0f67376a99d442319a0f033d4943be9d89a75d4e75e3df2b3cecf56ebec0"
 # Run image in interactive mode
 docker run -it $imageNameOrId bash
