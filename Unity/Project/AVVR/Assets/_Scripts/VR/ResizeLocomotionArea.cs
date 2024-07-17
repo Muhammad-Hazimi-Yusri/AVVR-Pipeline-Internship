cd7d16fd44f200573d45250b2f7d181c7315a5f6
Muhammad-Hazimi-Yusri <mhby1g21@soton.ac.uk>
2024-07-17 18:01:49 +0100

    bandaid fix on resizeLocomotionArea error with null check (have no clue what the correct behaviour should be

--- Unity/Project/AVVR/Assets/_Scripts/VR/ResizeLocomotionArea.cs
+++ Unity/Project/AVVR/Assets/_Scripts/VR/ResizeLocomotionArea.cs
@@ -6,61 +6,76 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class ResizeLocomotionArea : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void Resize()
     {
-        // get target object of scene (contains meshes)
-        GameObject targetObject = GameObject.Find("ModelWrapper").transform.GetChild(0).gameObject;
+        GameObject modelWrapper = GameObject.Find("ModelWrapper");
+        if (modelWrapper == null || modelWrapper.transform.childCount == 0)
+        {
+            Debug.LogWarning("ModelWrapper not found or has no children.");
+            return;
+        }
 
+        GameObject targetObject = modelWrapper.transform.GetChild(0).gameObject;
         if (targetObject != null)
         {
-            // set vertices
             SetQuadVertices(GetMeshBoundVertices(targetObject));
+        }
+        else
+        {
+            Debug.LogWarning("Target object not found in ModelWrapper.");
         }
     }
 
-    // gets the vectorised mesh bounds of the imported scene
     Vector3[] GetMeshBoundVertices(GameObject targetObject)
     {
-        // get all meshes in parent object
         MeshFilter[] meshFilters = targetObject.GetComponentsInChildren<MeshFilter>();
-        // holds mesh bounds
-        Bounds combinedBounds = new Bounds(meshFilters[0].transform.position, Vector3.zero);
-
-        // iterate through each sub-meshes bounds
-        foreach (MeshFilter meshFilter in meshFilters)
+        if (meshFilters.Length == 0)
         {
-            combinedBounds.Encapsulate(meshFilter.sharedMesh.bounds);
+            Debug.LogWarning("No MeshFilters found in target object.");
+            return new Vector3[4]; // Return default vertices
         }
 
-        // get array of world coords of 4 floor vertices
-        Vector3 bottomLeft = targetObject.transform.TransformPoint(
-            new Vector3(combinedBounds.min.x, 0f, combinedBounds.min.z));
-        Vector3 bottomRight = targetObject.transform.TransformPoint(
-            new Vector3(combinedBounds.min.x, 0f, combinedBounds.max.z));
-        Vector3 topLeft = targetObject.transform.TransformPoint(
-            new Vector3(combinedBounds.max.x, 0f, combinedBounds.min.z));
-        Vector3 topRight = targetObject.transform.TransformPoint(
-            new Vector3(combinedBounds.max.x, 0f, combinedBounds.max.z));
+        Bounds combinedBounds = new Bounds(meshFilters[0].transform.position, Vector3.zero);
+        foreach (MeshFilter meshFilter in meshFilters)
+        {
+            if (meshFilter.sharedMesh != null)
+            {
+                combinedBounds.Encapsulate(meshFilter.sharedMesh.bounds);
+            }
+        }
 
-        return new Vector3[] {bottomLeft, bottomRight, topLeft, topRight};
+        Vector3 bottomLeft = targetObject.transform.TransformPoint(new Vector3(combinedBounds.min.x, 0f, combinedBounds.min.z));
+        Vector3 bottomRight = targetObject.transform.TransformPoint(new Vector3(combinedBounds.min.x, 0f, combinedBounds.max.z));
+        Vector3 topLeft = targetObject.transform.TransformPoint(new Vector3(combinedBounds.max.x, 0f, combinedBounds.min.z));
+        Vector3 topRight = targetObject.transform.TransformPoint(new Vector3(combinedBounds.max.x, 0f, combinedBounds.max.z));
+
+        return new Vector3[] { bottomLeft, bottomRight, topLeft, topRight };
     }
 
     void SetQuadVertices(Vector3[] newVertices)
     {
-        // get quad's mesh
-        Mesh quadMesh = GetComponent<MeshFilter>().sharedMesh;
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        if (meshFilter == null || meshFilter.sharedMesh == null)
+        {
+            Debug.LogWarning("MeshFilter or sharedMesh is null on ResizeLocomotionArea.");
+            return;
+        }
 
-        // set vertices
+        Mesh quadMesh = meshFilter.sharedMesh;
         quadMesh.vertices = newVertices;
-
-        // recalculate mesh properties with updated vertices
         quadMesh.RecalculateNormals();
         quadMesh.RecalculateBounds();
         quadMesh.RecalculateTangents();
 
-        // update collider
-        GetComponent<MeshCollider>().sharedMesh = null;
-        GetComponent<MeshCollider>().sharedMesh = quadMesh;
+        MeshCollider meshCollider = GetComponent<MeshCollider>();
+        if (meshCollider != null)
+        {
+            meshCollider.sharedMesh = null;
+            meshCollider.sharedMesh = quadMesh;
+        }
+        else
+        {
+            Debug.LogWarning("MeshCollider not found on ResizeLocomotionArea.");
+        }
     }
 }
