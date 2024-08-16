65d181e2813e4408174f7884a736cb5586ab794d
Muhammad-Hazimi-Yusri <mhby1g21@soton.ac.uk>
2024-08-16 13:17:40 +0100

    testing reflection mix level 2 (even worse, higher noise floor level)

--- Atiyeh-RIR-evaluation-Matlab/KTv2.m
+++ Atiyeh-RIR-evaluation-Matlab/KTv2.m
@@ -2,9 +2,9 @@ addpath 'IoSR Toolbox' 'octave'
 
 % KT
 [LS2_sweep, fs2] = audioread("sounds/KT_GDP/RIR_KT_Unity_bf.wav");
-[sweep, fsst] = audioread("sounds/KT_GDP/sine_sweep_16bit.wav");
+[sweep, fsst] = audioread("sounds/KT_GDP/sine_sweep_16bit_full.wav");
 [RT60, DRR, C50, Cfs, EDT] = ...
-iosr.acoustics.irStats("sounds/KT_GDP/RIR_KT_Unity_bf.wav",'graph', true, 'spec', 'full', 'y_fit', [-5 -35]);
+iosr.acoustics.irStats("sounds/KT_GDP/RIR_KT_Unity_bf.wav",'graph', true, 'spec', 'full', 'y_fit', [-5 -15]);
 
 % Calculating Mean Values
 mean_RT60 = mean(RT60(3:8));
